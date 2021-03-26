2a28c3d131e40831255fb27d99ff118dd8218cb9
kingcobra2468 <eriktyryshkin@gmail.com>
2021-03-25 23:52:37 -0700

    changed box ui to rounded corners + added dark mode

--- app/static/js/gallery-view.js
+++ app/static/js/gallery-view.js
@@ -1,7 +1,9 @@
+'use strict';
 import {set_view_text} from './config-box.js';
 
+// on page laod event
 $(document).ready(function () {
 
-    $("#switch-view-btn").attr("href", "/library/list-view");
+    $('#switch-view-btn').attr('href', '/library/list-view');
     set_view_text('List View');
 });
