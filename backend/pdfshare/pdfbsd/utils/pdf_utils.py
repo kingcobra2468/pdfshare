933172f9384d54b47af76bccce2fdb0b55692964
erik <eriktyryshkin@gmail.com>
2022-11-29 23:19:51 -0800

    Fix name escaping and remove old png cover

--- backend/pdfshare/pdfbsd/utils/pdf_utils.py
+++ backend/pdfshare/pdfbsd/utils/pdf_utils.py
@@ -17,13 +17,10 @@ def create_pdf_cover(pdf_dir, covers_dir, pdf_name):
         int: Result of the internal system subprocess.
         0 if no errors and non-zero value upon failure.
     """
-    pdf_cover_path_png = shlex.quote(
-        os.path.join(covers_dir, f'{pdf_name}.png'))
-    pdf_cover_path_jpeg = shlex.quote(
-        os.path.join(covers_dir, f'{pdf_name}.jpeg'))
+    pdf_cover_path_png = os.path.join(covers_dir, f'{pdf_name}.png')
+    pdf_cover_path_jpeg = os.path.join(covers_dir, f'{pdf_name}.jpeg')
 
-    status = system(f'pdftoppm -f 1 -l 1 -png {os.path.join(pdf_dir, f"{pdf_name}.pdf")} >' +
-                    f'{pdf_cover_path_png}')
+    status = system(f'pdftoppm -f 1 -l 1 -png {shlex.quote(os.path.join(pdf_dir, f"{pdf_name}.pdf"))} > {shlex.quote(pdf_cover_path_png)}')
 
     if status:
         return status
@@ -31,6 +28,7 @@ def create_pdf_cover(pdf_dir, covers_dir, pdf_name):
     img = Image.open(pdf_cover_path_png)
     img.save(pdf_cover_path_jpeg, optimize=True, quality=70)
 
+    os.remove(pdf_cover_path_png)
 
 def extract_pdf_name(path):
     """Extracts the pdf filename from a given input path.
